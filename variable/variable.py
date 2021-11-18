a9e699d8908e3000de59d4238a451e4e92568568
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-11-18 09:31:37 +0600

    data type

---
+++ variable/variable.py
@@ -0,0 +1,32 @@
+x= 5
+x= 'shafikul'
+print(type(x))
+
+
+y= 5
+print(type(y))
+
+z= 5.5
+print(type(z))
+
+
+str = 'AppDividend'
+print(type(str))
+
+int = 123
+print(type(int))
+
+float = 21.19
+print(type(float))
+
+negative = -19
+print(type(negative))
+
+dictionary = {'blog':'AppDividend'}
+print(type(dictionary))
+
+list = [1, 2, 3]
+print(type(list))
+
+tuple = (19, 21, 46)
+print(type(tuple))

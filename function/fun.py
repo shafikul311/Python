3f36532377bfe4febb60108887c7c6861c6651fe
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-11-25 09:33:45 +0600

    py function

---
+++ function/fun.py
@@ -0,0 +1,6 @@
+##in python function are defined using def  
+
+def my_function():
+    print("hi i write first function using python")
+
+my_function()
---
+++ pyOperator/div.py
@@ -0,0 +1,12 @@
+x = 10
+y = 2
+
+z= x // y 
+print(z)
+
+a = z + 500
+print(a)
+
+c = "apple" and  "banana" 
+
+print(c)
